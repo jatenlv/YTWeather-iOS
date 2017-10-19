b1051c7b21e695210780848b770789dabd9cd54e
admin <chaoyue.liu@chinaredstar.com>
2017-10-19 16:58:59 +0800

    手势

--- YTWeather/YTWeather/MainViewController/Controller/YTMainViewController.m
+++ YTWeather/YTWeather/MainViewController/Controller/YTMainViewController.m
@@ -14,6 +14,8 @@
 
 #import "YTMainRequestNetworkTool.h"
 
+#define kSlideWidthScale 0.6
+
 @interface YTMainViewController ()
 <
 YTMainViewDelegate
@@ -24,6 +26,7 @@ YTMainViewDelegate
 @property (nonatomic, strong) YTMainView *mainView;
 
 @property (nonatomic, strong) YTWeatherModel *weatherModel;
+@property (weak, nonatomic) IBOutlet UIView *leftSlideView;
 
 @end
 
@@ -38,6 +41,9 @@ YTMainViewDelegate
     [self.scrollView addSubview:mainView];
     [self setupView];
     self.weatherModel = [[YTWeatherModel alloc] init];
+    
+    [self addSlideGesture];
+    
 }
 
 - (void)setupView
@@ -47,7 +53,84 @@ YTMainViewDelegate
     [self.scrollView addSubview:self.mainView];
 
 }
+#pragma mark 添加左侧侧滑手势和轻扫手势
+- (void)addSlideGesture
+{
+    UIPanGestureRecognizer * pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(changeFrame:)];
+    [self.scrollView addGestureRecognizer:pan];
+    UISwipeGestureRecognizer * swipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(showSlideView:)];
+    swipe.direction = UISwipeGestureRecognizerDirectionLeft | UISwipeGestureRecognizerDirectionRight;
+    [self.scrollView addGestureRecognizer:swipe];
+    [pan requireGestureRecognizerToFail:swipe];
 
+    
+}
+- (void)changeFrame:(UIPanGestureRecognizer *)pan
+{
+    //相对偏移量
+    __block  CGFloat  translatePointX = [pan translationInView:self.scrollView].x;
+    CGFloat scrollX = self.scrollView.frame.origin.x;
+    
+    
+    // 左滑滑动范围
+    if(self.scrollView.frame.origin.x + translatePointX< 0)
+    {
+        translatePointX = -self.scrollView.frame.origin.x;
+        //右滑滑动范围
+    }else if(scrollX + translatePointX > kSlideWidthScale * self.scrollView.width) {
+        
+        translatePointX =  kSlideWidthScale * self.scrollView.width - self.scrollView.frame.origin.x;
+    }
+    self.scrollView.centerX += translatePointX;
+    self.leftSlideView.centerX += translatePointX;
+    if(pan.state == UIGestureRecognizerStateChanged)
+    {
+        NSLog(@"change");
+    }else if(pan.state == UIGestureRecognizerStateEnded)
+    {
+        if(scrollX >= kSlideWidthScale/2 * self.scrollView.width )
+        {
+            translatePointX = kSlideWidthScale * self.scrollView.width - scrollX;
+
+            [UIView animateWithDuration:0.35 animations:^{
+                self.scrollView.centerX += translatePointX;
+                self.leftSlideView.centerX += translatePointX;
+            }];
+        }
+        else {
+            translatePointX = -scrollX;
+            [UIView animateWithDuration:0.35 animations:^{
+                self.scrollView.centerX += translatePointX;
+                self.leftSlideView.centerX += translatePointX;
+            }];
+            
+        }
+    }
+   
+    [pan setTranslation:CGPointZero inView:self.scrollView];
+}
+#pragma mark 扫滑手势方法
+- (void)showSlideView:(UISwipeGestureRecognizer *)swip
+{
+    
+    switch (swip.direction) {
+        case UISwipeGestureRecognizerDirectionRight:
+            NSLog(@"123");
+            break;
+        case UISwipeGestureRecognizerDirectionLeft:
+            NSLog(@"left");
+            break;
+        case UISwipeGestureRecognizerDirectionUp:
+            NSLog(@"up");
+            break;
+        case UISwipeGestureRecognizerDirectionDown:
+            NSLog(@"down");
+            break;
+        default:
+            break;
+    }
+    
+}
 - (void)loadData
 {
     [YTMainRequestNetworkTool requestWeatherWithCityName:@"北京" andFinish:^(YTWeatherModel *model, NSError *error) {
