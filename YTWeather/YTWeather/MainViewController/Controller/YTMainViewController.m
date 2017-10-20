bf83474afbd1cec0c7a48d7d8499c00fcc49b227
jaten <375103374@qq.com>
2017-10-20 10:44:29 +0800

    city和weather的plist文件;2D

--- YTWeather/YTWeather/MainViewController/Controller/YTMainViewController.m
+++ YTWeather/YTWeather/MainViewController/Controller/YTMainViewController.m
@@ -22,12 +22,12 @@ YTMainViewDelegate
 >
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
 @property (nonatomic, strong) YTMainView *mainView;
 
-@property (nonatomic, strong) YTWeatherModel *weatherModel;
 @property (weak, nonatomic) IBOutlet UIView *leftSlideView;
 @property (nonatomic,assign) BOOL isShowSlide;
+
+@property (nonatomic, strong) YTWeatherModel *weatherModel;
 
 @end
 
@@ -40,49 +40,6 @@ YTMainViewDelegate
     [self addSlideGesture];
 
     self.weatherModel = [[YTWeatherModel alloc] init];
-    
-    
-//    [self test];
-    
-}
-
-- (void)test
-{
-    // 转array
-    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"condition-code" ofType:@"txt"];
-    NSString *str = [[NSString alloc] initWithContentsOfFile:plistPath encoding:NSUTF8StringEncoding error:nil];
-    NSArray *ary = [str componentsSeparatedByString:@"\n"];
-    NSMutableArray *bigAry = [NSMutableArray array];
-    for (int i=1; i<ary.count; i++) {
-        NSArray *smallAry = [ary[i] componentsSeparatedByString:@"    "];
-        NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-        [dic setObject:smallAry[0] forKey:@"cityCode"];
-        [dic setObject:smallAry[1] forKey:@"cityEnglishName"];
-        [dic setObject:smallAry[2] forKey:@"cityChineseName"];
-        [dic setObject:smallAry[3] forKey:@"countryCode"];
-        [dic setObject:smallAry[4] forKey:@"countryEnglishName"];
-        [dic setObject:smallAry[5] forKey:@"countryChineseName"];
-        [dic setObject:smallAry[6] forKey:@"provinceEnglishName"];
-        [dic setObject:smallAry[7] forKey:@"provinceChineseName"];
-        [dic setObject:smallAry[8] forKey:@"belongToCityEnglishName"];
-        [dic setObject:smallAry[9] forKey:@"belongToCityChineseName"];
-        [dic setObject:smallAry[10] forKey:@"cityLng"];
-        [dic setObject:smallAry[11] forKey:@"cityLat"];
-        [bigAry addObject:dic];
-    }
-    
-//    // 写文件
-//    NSArray *paths=NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
-//    NSString *plistPath1 = [paths objectAtIndex:0];
-//    NSLog(@"path ::: %@",plistPath);
-//    
-//    NSString *fileName = [plistPath1 stringByAppendingPathComponent:@"weatherCode.plist"];
-//    NSFileManager *fm = [NSFileManager defaultManager];
-//    if ([fm createFileAtPath:fileName contents:nil attributes:nil] ==YES) {
-//        
-//        [bigAry writeToFile:fileName atomically:YES];
-//        NSLog(@"文件写入完成");
-//    }
 }
 
 - (void)setupView
@@ -93,29 +50,21 @@ YTMainViewDelegate
     self.mainView.delegate = self;
     [self.scrollView addSubview:self.mainView];
 }
+
 #pragma mark 添加左侧侧滑手势和轻扫手势
+
 - (void)addSlideGesture
 {
     UIPanGestureRecognizer * pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(changeFrame:)];
     [self.scrollView addGestureRecognizer:pan];
-//    UISwipeGestureRecognizer * swipeLeft = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(showSlideView:)];
-//    swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
-//    [self.scrollView addGestureRecognizer:swipeLeft];
-//    UISwipeGestureRecognizer * swipeRight = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(showSlideView:)];
-//    [self.scrollView addGestureRecognizer:swipeRight];
-
-//    [swipeLeft requireGestureRecognizerToFail:pan];
-//    [swipeRight requireGestureRecognizerToFail:pan];
-
-    
 }
+
 - (void)changeFrame:(UIPanGestureRecognizer *)pan
 {
     //相对偏移量
     __block  CGFloat  translatePointX = [pan translationInView:self.scrollView].x;
     CGFloat scrollX = self.scrollView.frame.origin.x;
-    
-    
+
     // 左滑滑动范围
     if(self.scrollView.frame.origin.x + translatePointX< 0)
     {
@@ -154,52 +103,52 @@ YTMainViewDelegate
    
     [pan setTranslation:CGPointZero inView:self.scrollView];
 }
-//#pragma mark 扫滑手势方法
-//- (void)showSlideView:(UISwipeGestureRecognizer *)swip
-//{
-//    CGFloat offset = kSlideWidthScale*self.scrollView.width;
-//    switch (swip.direction) {
-//        case UISwipeGestureRecognizerDirectionRight:{
-//            if(!_isShowSlide)
-//            {
-//                [UIView animateWithDuration:0.35 animations:^{
-//                    self.scrollView.centerX += offset;
-//                    self.leftSlideView.centerX += offset;
-//                }];
-//                _isShowSlide = YES;
-//            }
-//            break;
-//        }
-//        case UISwipeGestureRecognizerDirectionLeft:
-//            if(_isShowSlide)
-//            {
-//                [UIView animateWithDuration:0.35 animations:^{
-//                    self.scrollView.centerX += -offset;
-//                    self.leftSlideView.centerX += -offset;
-//                }];
-//                _isShowSlide = NO;
-//            }
-//            break;
-//        case UISwipeGestureRecognizerDirectionUp:
-//            NSLog(@"up");
-//            break;
-//        case UISwipeGestureRecognizerDirectionDown:
-//            NSLog(@"down");
-//            break;
-//        default:
-//            break;
-//    }
-//
-//}
+
 - (void)loadData
 {
     [YTMainRequestNetworkTool requestWeatherWithCityName:@"北京" andFinish:^(YTWeatherModel *model, NSError *error) {
         [self.mainView.tableView.mj_header endRefreshing];
         if (!error) {
-            self.weatherModel = model; // 暂时无用
+//            self.weatherModel = model; // 暂时无用
             self.mainView.weatherModel = model;
         }
     }];
 }
+
+/*
+ * 暂时不用 以后如果需要转换再用 txt -> plist
+- (void)test
+{
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"condition-code" ofType:@"txt"];
+    NSString *str = [[NSString alloc] initWithContentsOfFile:plistPath encoding:NSUTF8StringEncoding error:nil];
+    NSArray *ary = [str componentsSeparatedByString:@"\n"];
+    NSMutableArray *bigAry = [NSMutableArray array];
+    for (int i=1; i<ary.count-1; i++) {
+        NSArray *smallAry = [ary[i] componentsSeparatedByString:@"    "];
+        NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+        [dic setObject:smallAry[0] forKey:@"cityCode"];
+        [dic setObject:smallAry[1] forKey:@"cityEnglishName"];
+        [dic setObject:smallAry[2] forKey:@"cityChineseName"];
+        [dic setObject:smallAry[3] forKey:@"countryCode"];
+        [dic setObject:smallAry[4] forKey:@"countryEnglishName"];
+        [dic setObject:smallAry[5] forKey:@"countryChineseName"];
+        [dic setObject:smallAry[6] forKey:@"provinceEnglishName"];
+        [dic setObject:smallAry[7] forKey:@"provinceChineseName"];
+        [dic setObject:smallAry[8] forKey:@"belongToCityEnglishName"];
+        [dic setObject:smallAry[9] forKey:@"belongToCityChineseName"];
+        [dic setObject:smallAry[10] forKey:@"cityLng"];
+        [dic setObject:smallAry[11] forKey:@"cityLat"];
+        [bigAry addObject:dic];
+    }
+    
+    NSArray *paths=NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
+    NSString *plistPath1 = [paths objectAtIndex:0];
+    NSString *fileName = [plistPath1 stringByAppendingPathComponent:@"cityCode.plist"];
+    NSFileManager *fm = [NSFileManager defaultManager];
+    if ([fm createFileAtPath:fileName contents:nil attributes:nil] ==YES) {
+        [bigAry writeToFile:fileName atomically:YES];
+        NSLog(@"文件写入完成");
+    }
+}*/
 
 @end
