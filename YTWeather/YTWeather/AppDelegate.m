c49ea7c469455022b869d639e01ff6c27a206b97
jaten <375103374@qq.com>
2017-11-28 10:47:42 +0800

    修改PrefixHeader

--- YTWeather/YTWeather/AppDelegate.m
+++ YTWeather/YTWeather/AppDelegate.m
@@ -27,18 +27,18 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 
     //当前版本号
-    NSString * curVersion = [NSBundle mainBundle].infoDictionary[@"CFBundleShortVersionString"];
+    NSString *curVersion = [NSBundle mainBundle].infoDictionary[@"CFBundleShortVersionString"];
     //保存的版本号
-    NSString * lastVersion = [[NSUserDefaults standardUserDefaults]objectForKey:ShortVersionKey];
+    NSString *lastVersion = [[NSUserDefaults standardUserDefaults]objectForKey:ShortVersionKey];
     
-    UIViewController * rootVC;
+    UIViewController *rootVC;
     //判断版本
-    if([lastVersion isEqualToString:curVersion]) {
+    if ([lastVersion isEqualToString:curVersion]) {
         //进入广告页
-        rootVC = [[YTLaunchADViewController alloc]init];
-    }else {
+        rootVC = [[YTLaunchADViewController alloc] init];
+    } else {
         //进入新特性
-        rootVC = [[YTNewFeatureViewController alloc]init];
+        rootVC = [[YTNewFeatureViewController alloc] init];
         //更新版本
         [[NSUserDefaults standardUserDefaults] setObject:curVersion forKey:ShortVersionKey];
         [[NSUserDefaults standardUserDefaults] synchronize];
@@ -51,40 +51,31 @@
 
 - (void)setupUMShare
 {
-    /* 打开调试日志 */
+    // 友盟初始化
     [[UMSocialManager defaultManager] openLog:YES];
-    /* 设置友盟appkey */
-    [[UMSocialManager defaultManager] setUmSocialAppkey:USHARE_DEMO_APPKEY];
+    
+    [[UMSocialManager defaultManager] setUmSocialAppkey:USHARE_APPKEY];
     
 //    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_WechatSession appKey:@"" appSecret:@"" redirectURL:nil];
     
-    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:@"1106466575"/*设置QQ平台的appID*/  appSecret:@"PZ8Fu8s6srt7BUjB" redirectURL:@""];
-    
-
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:QQ_APP_KEY appSecret:QQ_APP_SECRET redirectURL:@""];
     
 //    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Sina appKey:@"" appSecret:@"" redirectURL:nil];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
